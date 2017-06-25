21d46e004d39059d3b74df1e7be3ac3cfbdee5f9
Kavitha <bk.kavitha1@gmail.com>
2017-06-25 11:42:31 +0530

    Latest WAR to cloud

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -10,19 +10,15 @@ import com.glarimy.cmad.blogging.service.UserService;;
 
 public class UserTest {
 	UserService testuserservice = new UserService();
-
-	@Test
 	public void testUserInsert(){
 		User testuserdata = new User();
 		testuserdata.setEmailId("kavitakr@cisco.com").setFirstName("Kavitha").setId("kavitakr").setLastName("BK").setPassword("cmad");
 		testuserservice.create(testuserdata);
 	}
-	@Test
 	public void testreadAllUsers() throws DataNotFoundException, EntityException{
 		 List<User> users = testuserservice.readAllUsers();
 		 System.out.println(users);
 	}
-	@Test
 	public void testauthenticate() throws SecurityException, EntityException {
 		User user = testuserservice.authenticate("kavitakr", "cmad");
 		System.out.println("Firtst Name " + user.getFirstName());	
