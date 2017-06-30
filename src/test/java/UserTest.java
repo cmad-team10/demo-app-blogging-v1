b2fa285a333545dfdddada632289eac48c5f4d16
Kavitha <bk.kavitha1@gmail.com>
2017-06-30 09:41:06 +0530

    Add junit test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -9,17 +9,19 @@ import com.glarimy.cmad.blogging.service.UserService;;
 
 
 public class UserTest {
+	private UserService testuserservice = new UserService();
 	@Test
-	UserService testuserservice = new UserService();
 	public void testUserInsert(){
 		User testuserdata = new User();
 		testuserdata.setEmailId("kavitakr@cisco.com").setFirstName("Kavitha").setId("kavitakr").setLastName("BK").setPassword("cmad");
 		testuserservice.create(testuserdata);
 	}
+	@Test
 	public void testreadAllUsers() throws DataNotFoundException, EntityException{
 		 List<User> users = testuserservice.readAllUsers();
 		 System.out.println(users);
 	}
+	@Test
 	public void testauthenticate() throws SecurityException, EntityException {
 		User user = testuserservice.authenticate("kavitakr", "cmad");
 		System.out.println("Firtst Name " + user.getFirstName());	
